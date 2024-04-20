36af259f1cbed41c2dbd967cf5cd07af82aced4c
Allen Forjaz <91799573+allenforjaz@users.noreply.github.com>
2024-04-20 04:01:37 +0100

    Data formatting and reading

--- index.js
+++ index.js
@@ -12,12 +12,23 @@ var svg = d3.select("#my_dataviz")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
-//Read the data
-d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv",
+//Function to convert quarterly time-period
+function convert_year_quarter_to_date(quarter,year) {
+    quarter_number = Number(quarter.replace("Q", ""))
+    quarter_month = (quarter_number -1) * 3 + 1
+    return `${year}-${quarter_month}-01`
+}
 
-  // When reading the csv, I must format variables:
+//Read the data
+d3.csv("https://raw.githubusercontent.com/allenforjaz/sdv-assignment/main/data/house_prices_recorded_income_data.csv",
+  // Format data
   function(d){
-    return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
+    converted_date = convert_year_quarter_to_date(d.quarter,d.year)
+    converted_house_price = parseFloat(d.avg_house_price.replace(',',''));
+    converted_recorded_income = parseFloat(d.avg_recorded_income.replace(',',''));
+    const dateParser = d3.timeParse("%Y-%m-%d")
+    console.log(dateParser(converted_date))
+    return { date : dateParser(converted_date), house_price : converted_house_price, recorded_income : converted_recorded_income }
   },
 
   // Now I can use this dataset:
@@ -33,7 +44,7 @@ d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_data
 
     // Add Y axis
     var y = d3.scaleLinear()
-      .domain([0, d3.max(data, function(d) { return +d.value; })])
+      .domain([0, d3.max(data, function(d) { return +d.house_price; })])
       .range([ height, 0 ]);
     svg.append("g")
       .call(d3.axisLeft(y));
@@ -46,7 +57,7 @@ d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_data
       .attr("stroke-width", 1.5)
       .attr("d", d3.line()
         .x(function(d) { return x(d.date) })
-        .y(function(d) { return y(d.value) })
+        .y(function(d) { return y(d.house_price) })
         )
 
 })
